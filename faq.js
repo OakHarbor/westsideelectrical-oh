afae0c7e509299fbf8dfce4d3eada21dec44668a
OakHarbor <ohwdinfo@gmail.com>
2021-09-16 21:20:43 -0700

    first commit

---
+++ faq.js
@@ -0,0 +1,120 @@
+ // FAQ js
+
+ const faqItem1 = document.querySelector("#faq .item-1");
+ const faqItem2 = document.querySelector("#faq .item-2");
+ const faqItem3 = document.querySelector("#faq .item-3");
+ const faqItem4 = document.querySelector("#faq .item-4");
+ const faqItem5 = document.querySelector("#faq .item-5");
+ const faqItem6 = document.querySelector("#faq .item-6");
+ const faqItem7 = document.querySelector("#faq .item-7");
+ const faqItem8 = document.querySelector("#faq .item-8");
+ const faqItem9 = document.querySelector("#faq .item-9");
+ const faqItem10 = document.querySelector("#faq .item-10");
+ const faqItem11 = document.querySelector("#faq .item-11");
+ const faqItem12 = document.querySelector("#faq .item-12");
+ const faqItem13 = document.querySelector("#faq .item-13");
+ const faqItem14 = document.querySelector("#faq .item-14");
+
+ const faqItem1H3 = document.querySelector("#faq .item-1 h3");
+ const faqItem2H3 = document.querySelector("#faq .item-2 h3");
+ const faqItem3H3 = document.querySelector("#faq .item-3 h3");
+ const faqItem4H3 = document.querySelector("#faq .item-4 h3");
+ const faqItem5H3 = document.querySelector("#faq .item-5 h3");
+ const faqItem6H3 = document.querySelector("#faq .item-6 h3");
+ const faqItem7H3 = document.querySelector("#faq .item-7 h3");
+ const faqItem8H3 = document.querySelector("#faq .item-8 h3");
+ const faqItem9H3 = document.querySelector("#faq .item-9 h3");
+ const faqItem10H3 = document.querySelector("#faq .item-10 h3");
+ const faqItem11H3 = document.querySelector("#faq .item-11 h3");
+ const faqItem12H3 = document.querySelector("#faq .item-12 h3");
+ const faqItem13H3 = document.querySelector("#faq .item-13 h3");
+ const faqItem14H3 = document.querySelector("#faq .item-14 h3");
+
+ const faqItem1Ex = document.querySelector("#faq .item-1 .faq-extended-content");
+ const faqItem2Ex = document.querySelector("#faq .item-2 .faq-extended-content");
+ const faqItem3Ex = document.querySelector("#faq .item-3 .faq-extended-content");
+ const faqItem4Ex = document.querySelector("#faq .item-4 .faq-extended-content");
+ const faqItem5Ex = document.querySelector("#faq .item-5 .faq-extended-content");
+ const faqItem6Ex = document.querySelector("#faq .item-6 .faq-extended-content");
+ const faqItem7Ex = document.querySelector("#faq .item-7 .faq-extended-content");
+ const faqItem8Ex = document.querySelector("#faq .item-8 .faq-extended-content");
+ const faqItem9Ex = document.querySelector("#faq .item-9 .faq-extended-content");
+ const faqItem10Ex = document.querySelector("#faq .item-10 .faq-extended-content");
+ const faqItem11Ex = document.querySelector("#faq .item-11 .faq-extended-content");
+ const faqItem12Ex = document.querySelector("#faq .item-12 .faq-extended-content");
+ const faqItem13Ex = document.querySelector("#faq .item-13 .faq-extended-content");
+ const faqItem14Ex = document.querySelector("#faq .item-14 .faq-extended-content");
+
+ const faqP = document.querySelector("#faq .faq-item p");
+ const faqExtended = document.querySelector("#faq .faq-item .faq-extended-content");
+ 
+
+ faqItem1.addEventListener('click', () => {
+   faqItem1Ex.classList.toggle('clicked');
+   faqItem1H3.classList.toggle('clicked');
+ })
+ 
+ faqItem2.addEventListener('click', () => {
+   faqItem2Ex.classList.toggle('clicked');
+   faqItem2H3.classList.toggle('clicked');
+ })
+
+ faqItem3.addEventListener('click', () => {
+   faqItem3Ex.classList.toggle('clicked');
+   faqItem3H3.classList.toggle('clicked');
+ })
+
+ faqItem4.addEventListener('click', () => {
+   faqItem4Ex.classList.toggle('clicked');
+   faqItem4H3.classList.toggle('clicked');
+ })
+
+ faqItem5.addEventListener('click', () => {
+   faqItem5Ex.classList.toggle('clicked');
+   faqItem5H3.classList.toggle('clicked');
+ })
+
+ faqItem6.addEventListener('click', () => {
+   faqItem6Ex.classList.toggle('clicked');
+   faqItem6H3.classList.toggle('clicked');
+ })
+
+ faqItem7.addEventListener('click', () => {
+   faqItem7Ex.classList.toggle('clicked');
+   faqItem7H3.classList.toggle('clicked');
+ })
+
+ faqItem8.addEventListener('click', () => {
+   faqItem8Ex.classList.toggle('clicked');
+   faqItem8H3.classList.toggle('clicked');
+ })
+
+ faqItem9.addEventListener('click', () => {
+   faqItem9Ex.classList.toggle('clicked');
+   faqItem9H3.classList.toggle('clicked');
+ })
+
+ faqItem10.addEventListener('click', () => {
+   faqItem10Ex.classList.toggle('clicked');
+   faqItem10H3.classList.toggle('clicked');
+ })
+
+ faqItem11.addEventListener('click', () => {
+   faqItem11Ex.classList.toggle('clicked');
+   faqItem11H3.classList.toggle('clicked');
+ })
+
+ faqItem12.addEventListener('click', () => {
+   faqItem12Ex.classList.toggle('clicked');
+   faqItem12H3.classList.toggle('clicked');
+ })
+
+ faqItem13.addEventListener('click', () => {
+   faqItem13Ex.classList.toggle('clicked');
+   faqItem13H3.classList.toggle('clicked');
+ })
+
+ faqItem14.addEventListener('click', () => {
+   faqItem14Ex.classList.toggle('clicked');
+   faqItem14H3.classList.toggle('clicked');
+ })
